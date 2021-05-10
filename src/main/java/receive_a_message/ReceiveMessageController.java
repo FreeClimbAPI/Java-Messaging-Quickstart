fd4151c3002693c99e934908bdf5b2dc1d3d4140
Katie <kohearn@vailsys.com>
2021-05-10 13:07:34 -0500

    find and replace auth token -> api key

--- src/main/java/receive_a_message/ReceiveMessageController.java
+++ src/main/java/receive_a_message/ReceiveMessageController.java
@@ -12,13 +12,13 @@ public class ReceiveMessageController {
   private final String fromNumber = System.getenv("FREECLIMB_PHONE_NUMBER");
   private final String toNumber = System.getenv("TO_PHONE_NUMBER");
   private final String accountId = System.getenv("ACCOUNT_ID");
-  private final String authToken = System.getenv("AUTH_TOKEN");
+  private final String apiKey = System.getenv("API_KEY");
 
   @RequestMapping(value = { "/InboundSms" }, method = RequestMethod.POST)
   public void inboundSms(@RequestBody String body) {
     FreeClimbClient client;
     try {
-      client = new FreeClimbClient(accountId, authToken);
+      client = new FreeClimbClient(accountId, apiKey);
       client.messages.create(fromNumber, toNumber, "Hello from the FreeClimb API!");
     } catch (FreeClimbException e) {
       // Handle Error

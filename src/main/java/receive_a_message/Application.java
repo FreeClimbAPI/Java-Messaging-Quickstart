d797025f0e13f9b60ac9542a2bb85c1586a5b645
Bhargav Ganesh <bganesh@vailsys.com>
2024-04-30 14:56:51 -0500

    Added env var check

--- src/main/java/receive_a_message/Application.java
+++ src/main/java/receive_a_message/Application.java
@@ -2,12 +2,18 @@ package main.java.receive_a_message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    if (dotenv.get("ACCOUNT_ID") == null || dotenv.get("API_KEY") == null || dotenv.get("FREECLIMB_NUMBER") == null){
+      System.err.println("ERROR: ENVIRONMENT VARIABLES ARE NOT SET. PLEASE SET ALL ENVIRONMMENT VARIABLES AND RETRY.");
+      return;
+    }
     SpringApplication.run(Application.class, args);
   }
 }
